820c021d06899e727e9db741a2f79f4f1a024e38
Nisheeth Bandaru <nisheeth@gatech.edu>
2016-03-19 18:43:45 -0400

    graph ready
    created the graph

--- src/main/scala/edu/gatech/cse8803/graphconstruct/GraphLoader.scala
+++ src/main/scala/edu/gatech/cse8803/graphconstruct/GraphLoader.scala
@@ -18,19 +18,23 @@ object GraphLoader {
     * */
   def load(patients: RDD[PatientProperty], labResults: RDD[LabResult],
            medications: RDD[Medication], diagnostics: RDD[Diagnostic]): Graph[VertexProperty, EdgeProperty] = {
-    /*
-    val labResultsLatest = labResults.map(l => (l.patientID, l))
-      .reduceByKey((l1 , l2) => {if (l1.date > l2.date) l1 else l2})
-      .map(l => l._2)
 
-    val diaglatest = diagnostics.map(l => (l.patientID, l))
-      .reduceByKey((l1 , l2) => {if (l1.date > l2.date) l1 else l2})
-      .map(l => l._2)
+    val sc = patients.sparkContext
 
-    val medlatest = medications.map(l => (l.patientID, l))
+    val labResultsLatest = labResults.map(l => ((l.patientID, l.labName), l))
       .reduceByKey((l1 , l2) => {if (l1.date > l2.date) l1 else l2})
-      .map(l => l._2)
+      .map(l => LabResult(l._2.patientID, l._2.date, l._2.labName, l._2.value))
 
+    val diaglatest = diagnostics.map(l => ((l.patientID, l.icd9code), l))
+      .reduceByKey((l1 , l2) => {if (l1.date > l2.date) l1 else l2})
+      .map(l => Diagnostic(l._2.patientID, l._2.date, l._2.icd9code, l._2.sequence))
+
+    val medlatest = medications.map(l => ((l.patientID, l.medicine), l))
+      .reduceByKey((l1 , l2) => {if (l1.date > l2.date) l1 else l2})
+      .map(l => Medication(l._2.patientID, l._2.date, l._2.medicine))
+
+
+    /** Check the counts of taking only the latest events
     println("patient count: ", patients.count())
     println("lab events count: ", labResults.count())
     println("diag events count: ", diagnostics.count())
@@ -39,7 +43,7 @@ object GraphLoader {
     println("diag results latest count: ", diaglatest.count())
     println("med results latest count: ", medlatest.count())
 
-    */
+      */
 
     /** HINT: See Example of Making Patient Vertices Below */
     val vertexPatient: RDD[(VertexId, VertexProperty)] = patients
@@ -92,8 +96,7 @@ object GraphLoader {
       .map{case(medicine, index) => (index, MedicationProperty(medicine))}
       .asInstanceOf[RDD[(VertexId, VertexProperty)]]
 
-
-
+    /** Some prints
     println("diag vertex count: ", vertexDiag.count())
     println("lab vertex count: ", vertexLab.count())
     println("med vertex count: ", vertexMed.count)
@@ -101,6 +104,15 @@ object GraphLoader {
     println("lab vertex id start: ", startIndex_lab)
     println("med vertex id start: ", startIndex_med)
     println("med vertex id end: ", endIndex_med)
+      */
+
+    val diagnostic2VertexId = diagVertexIdRDD.collect.toMap
+    val lab2VertexId = labVertexIdRDD.collect.toMap
+    val med2VertexId = medVertexRDD.collect.toMap
+
+    val bcDiagnostic2VertexId = sc.broadcast(diagnostic2VertexId)
+    val bcMedication2VertexId = sc.broadcast(med2VertexId)
+    val bcLab2VertexId = sc.broadcast(lab2VertexId)
 
 
     /** HINT: See Example of Making PatientPatient Edges Below
@@ -114,10 +126,65 @@ object GraphLoader {
         Edge(p.patientID.toLong, p.patientID.toLong, SampleEdgeProperty("sample").asInstanceOf[EdgeProperty])
       })
 
+    val edgePatientDiagnostic: RDD[Edge[EdgeProperty]] = diaglatest
+      .map{
+        diag: Diagnostic => Edge(
+          diag.patientID.toLong,
+          bcDiagnostic2VertexId.value(diag.icd9code),
+          PatientDiagnosticEdgeProperty(Diagnostic(diag.patientID, diag.date, diag.icd9code, diag.sequence)).asInstanceOf[EdgeProperty]
+          )
+      }
+    val edgePatientDiagnostic2: RDD[Edge[EdgeProperty]] = diaglatest
+      .map{
+        diag: Diagnostic => Edge(
+          bcDiagnostic2VertexId.value(diag.icd9code),
+          diag.patientID.toLong,
+          PatientDiagnosticEdgeProperty(Diagnostic(diag.patientID, diag.date, diag.icd9code, diag.sequence)).asInstanceOf[EdgeProperty]
+        )
+      }
 
+    val edgePatientMedication: RDD[Edge[EdgeProperty]] = medlatest
+      .map{
+        med: Medication => Edge(
+          med.patientID.toLong,
+          bcMedication2VertexId.value(med.medicine),
+          PatientMedicationEdgeProperty(Medication(med.patientID, med.date, med.medicine)).asInstanceOf[EdgeProperty]
+        )
+      }
+    val edgePatientMedication2: RDD[Edge[EdgeProperty]] = medlatest
+      .map{
+        med: Medication => Edge(
+          bcMedication2VertexId.value(med.medicine),
+          med.patientID.toLong,
+          PatientMedicationEdgeProperty(Medication(med.patientID, med.date, med.medicine)).asInstanceOf[EdgeProperty]
+        )
+      }
+
+    val edgePatientLab: RDD[Edge[EdgeProperty]] = labResultsLatest
+      .map{
+        lab: LabResult => Edge(
+          lab.patientID.toLong,
+          bcLab2VertexId.value(lab.labName),
+          PatientLabEdgeProperty(LabResult(lab.patientID, lab.date, lab.labName, lab.value)).asInstanceOf[EdgeProperty]
+        )
+      }
+    val edgePatientLab2: RDD[Edge[EdgeProperty]] = labResultsLatest
+      .map{
+        lab: LabResult => Edge(
+          bcLab2VertexId.value(lab.labName),
+          lab.patientID.toLong,
+          PatientLabEdgeProperty(LabResult(lab.patientID, lab.date, lab.labName, lab.value)).asInstanceOf[EdgeProperty]
+        )
+      }
+
+    // union all the vertices
+    val allVertices = sc.union(vertexPatient, vertexDiag, vertexMed, vertexLab)
+
+    // union all the edges
+    val allEdges = sc.union(edgePatientDiagnostic, edgePatientDiagnostic2, edgePatientLab, edgePatientLab2, edgePatientMedication, edgePatientMedication2)
 
     // Making Graph
-    val graph: Graph[VertexProperty, EdgeProperty] = Graph(vertexPatient, edgePatientPatient)
+    val graph: Graph[VertexProperty, EdgeProperty] = Graph(allVertices, allEdges)
 
     graph
   }
