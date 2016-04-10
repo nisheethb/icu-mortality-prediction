9fa2d6d4dc501943ee92c5caffab9d9231fb967a
Nisheeth Bandaru <nisheeth@gatech.edu>
2016-04-10 17:31:15 -0400

    trying out lda

--- src/main/scala/edu/gatech/cse8803/features/FeatureConstruction.scala
+++ src/main/scala/edu/gatech/cse8803/features/FeatureConstruction.scala
@@ -5,13 +5,15 @@ package edu.gatech.cse8803.features
   * */
 
 import edu.gatech.cse8803.model._
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
+import scala.collection.mutable
+import org.apache.spark.mllib.clustering.{DistributedLDAModel, LDA}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.feature.IDF
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
 import java.util.Date
 
@@ -101,20 +103,61 @@ object FeatureConstruction {
     labeled
   }
 
-  def applytfidf(icuNotes:RDD[IcuEvent]): RDD[Vector] = {
+  def applytfidf(icuNotes:RDD[IcuEvent]): List[Int] = {
 
     val sc = icuNotes.sparkContext
-    // Load documents (one per line).
-    val documents: RDD[Seq[String]] = sc.textFile("data/sampledata.txt").map(_.split(" ").toSeq)
+    val notes = icuNotes.map( f => f.text)
 
-    val hashingTF = new HashingTF()
-    val tf: RDD[Vector] = hashingTF.transform(documents)
-    tf.cache()
-    val idf = new IDF().fit(tf)
-    val tfidf: RDD[Vector] = idf.transform(tf)
+    val corpus: RDD[String] = notes
+    // Split each document into a sequence of terms (words)
+    val tokenized: RDD[Seq[String]] =
+      corpus.map(_.toLowerCase.split("\\s")).map(_.filter(_.length > 3).filter(_.forall(java.lang.Character.isLetter)))
 
-    tfidf
+    // Choose the vocabulary.
+    //   termCounts: Sorted list of (term, termCount) pairs
+    val termCounts: Array[(String, Long)] =
+      tokenized.flatMap(_.map(_ -> 1L)).reduceByKey(_ + _).collect().sortBy(-_._2)
+    //   vocabArray: Chosen vocab (removing common terms)
+    val numStopwords = 20
+    val vocabArray: Array[String] =
+      termCounts.takeRight(termCounts.size - numStopwords).map(_._1)
+    //   vocab: Map term -> term index
+    val vocab: Map[String, Int] = vocabArray.zipWithIndex.toMap
 
+    val vocabsize = vocab.values.size
+    println("vocab size ", vocabsize)
+
+    // Convert documents into term count vectors
+    val documents: RDD[(Long, Vector)] =
+      tokenized.zipWithIndex.map { case (tokens, id) =>
+        val counts = new mutable.HashMap[Int, Double]()
+        tokens.foreach { term =>
+          if (vocab.contains(term)) {
+            val idx = vocab(term)
+            counts(idx) = counts.getOrElse(idx, 0.0) + 1.0
+          }
+        }
+        (id, Vectors.sparse(vocab.size, counts.toSeq))
+      }
+
+    // Set LDA parameters
+    val numTopics = 10
+    val lda = new LDA().setK(numTopics).setMaxIterations(10)
+
+    val ldaModel = lda.run(documents)
+    val avgLogLikelihood = ldaModel.asInstanceOf[DistributedLDAModel].logLikelihood / documents.count()
+
+    // Print topics, showing top-weighted 10 terms for each topic.
+    val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = 10)
+    topicIndices.foreach { case (terms, termWeights) =>
+      println("TOPIC:")
+      terms.zip(termWeights).foreach { case (term, weight) =>
+        println(s"${vocabArray(term.toInt)}\t$weight")
+      }
+      println()
+    }
+
+    List(1,2,3)
   }
 
 }
